02c8e6152f55736b4f0cf3875083d3c7c75044f5
Juan Rojas <poseidonjm@gmal.com>
2019-01-29 19:21:11 -0500

    start

---
+++ Allfiles/20486C/Mod06/Democode/OperasWebsites_06_begin/OperasebSitesTest/HomeControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OperasebSitesTest
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Test_Index_Return_View()
+        {
+            //arrange
+
+
+            //act
+
+            //accept
+        }
+    }
+}
---
+++ Allfiles/20486C/Mod06/Democode/OperasWebsites_06_begin/OperasebSitesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OperasebSitesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OperasebSitesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b64cfc9-7578-46c2-821b-e9773be900f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
